c08c2c5675d68669701a5b56f466039508ba5f3a
Kakon Ray <thisiskakonray@gmail.com>
2022-05-16 18:28:22 +0600

    dashboard and jwt token

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Running ginius server");
+  res.send("Running Doctor Portal server");
 });
 
 // db users
@@ -36,6 +36,7 @@ async function run() {
       .db("doctorPortal")
       .collection("appointmentCollection");
     const servicesCollection = client.db("doctorPortal").collection("services");
+    const userCollection = client.db("doctorPortal").collection("user");
 
     // add api . add appointed qury
     app.post("/appointment", async (req, res) => {
@@ -43,8 +44,8 @@ async function run() {
       // console.log(appointment);
       const query = {
         tretmentName: appointment.tretmentName,
-        data: appointment.data,
-        name: appointment.name,
+        date: appointment.date,
+        email: appointment.email,
       };
 
       const exist = await appointmentCollection.findOne(query);
@@ -57,6 +58,31 @@ async function run() {
       return res.send({ success: true, result });
     });
 
+    // create api in dashboart doctor appointment
+
+    app.get("/myappointment", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const appointment = await appointmentCollection.find(query).toArray();
+      res.send(appointment);
+    });
+
+    // user update api create
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      // upsert true dile social account diye login korrar somoy ekbar er besi email add hobe na
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = userCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    });
+
     // get api all services
     app.get("/services", async (req, res) => {
       const query = {};
@@ -65,21 +91,24 @@ async function run() {
       res.send(result);
     });
 
+    // abailable slots in the appointment
+
     app.get("/abailable", async (req, res) => {
-      const date = req.query.data || "May 15, 2022";
+      const date = req.query.date;
+      console.log(date);
       const services = await servicesCollection.find().toArray();
       const query = { date: date };
       const booking = await appointmentCollection.find(query).toArray();
 
       services.forEach((service) => {
         const serviceBooking = booking.filter(
-          (b) => b.tretmentName === service.name
+          (b) => b.tretmentName == service.name
         );
-        const booked = serviceBooking.map((item) => item.slot);
+        const booked = serviceBooking.map((item) => item.slots);
         const abailable = service.slots.filter(
           (item) => !booked.includes(item)
         );
-        service.abailable = abailable;
+        service.slots = abailable;
       });
       res.send(services);
     });
